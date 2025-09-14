a807334e29680114eaa2af4b8438f99232f8536a
Emy-gold <imanelgarate@gmail.com>
2025-09-14 22:21:38 +0100

    bfljb

--- .history/django-models/LibraryProject/LibraryProject/urls_20250914222055.py
+++ .history/django-models/LibraryProject/LibraryProject/urls_20250914222055.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-from django.views import list_books
+from .views import list_books
 
 urlpatterns = [
     path('admin/', admin.site.urls),
