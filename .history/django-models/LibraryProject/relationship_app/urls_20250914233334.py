2561eb8a127395cee32df3f07558d345ca4544b3
Emy-gold <imanelgarate@gmail.com>
2025-09-14 23:45:04 +0100

    hfvkhdf

--- .history/django-models/LibraryProject/relationship_app/urls_20250914233334.py
+++ .history/django-models/LibraryProject/relationship_app/urls_20250914233334.py
@@ -9,16 +9,11 @@ from django.urls import path
 from .views import add_book, edit_book, delete_book
 
 urlpatterns = [
-    # Books & Library
-    path("books/", list_books, name="list_books"),
-    path("library/<int:pk>/", LibraryDetailView.as_view(), name="library_detail"),
-
-    # Role-based dashboards
+    path("books/", list_books, name="list_books"),  # FBV
+    path("library/<int:pk>/", LibraryDetailView.as_view(), name="library_detail"),  # CBV
     path("admin-dashboard/", admin_view, name="admin_view"),
     path("librarian-dashboard/", librarian_view, name="librarian_view"),
     path("member-dashboard/", member_view, name="member_view"),
-
-    # Permission-protected book actions
     path("books/add/", add_book, name="add_book"),
     path("books/<int:pk>/edit/", edit_book, name="edit_book"),
     path("books/<int:pk>/delete/", delete_book, name="delete_book"),
